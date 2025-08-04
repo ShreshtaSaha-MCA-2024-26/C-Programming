9b2b706323367658f12eff0b88e1484c583a79e5
ShreshtaSaha-MCA-2024-26 <shreshtasaha21@gmail.com>
2025-08-05 03:06:56 +0530

    Create Program9.cpp

---
+++ Assignment1/Program9.cpp
@@ -0,0 +1,10 @@
+// Write a program to print your name without using any semicolons in the program.
+
+#include<stdio.h>
+int main()
+{
+     printf("\n**Print Name Without Any Semicolon**\n");
+     printf("Name : Shreshta Saha, Class : MCA1B, Roll : 40\n");
+     if(printf("My name is Shreshta Saha\n")){}
+     return 0;
+}
