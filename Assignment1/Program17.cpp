769a169fafbb1bf7c5147b09f7002d3d7e382ec2
ShreshtaSaha-MCA-2024-26 <shreshtasaha21@gmail.com>
2025-08-07 01:52:38 +0530

    Create Program17.cpp

---
+++ Assignment1/Program17.cpp
@@ -0,0 +1,20 @@
+// Write a C program to check whether a year is a leap year or not.
+
+#include <stdio.h>
+int main()
+{
+   int year;
+   printf("\n\n***C program to check whether a year is a leap year or not*\n");
+   printf("Name: Shreshta Saha, Class: MCA1B, Roll-40\n\n");
+   printf("Enter a year: ");
+   scanf("%d", &year);
+   if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+   {
+      printf("%d is a leap year.\n", year);
+   }
+   else
+   {
+      printf("%d is not a leap year.\n", year);
+   }
+   return 0;
+}
